54bcb4c25009e70f605865851c6d6dc32f6bce4a
Lorenzo <cingano.lorenzo2002@gmail.com>
2022-04-03 12:05:34 +0200

    Initial commit

---
+++ src/main/java/me/lorenzo/guimanager/opener/SpecialInventoryOpener.java
@@ -0,0 +1,43 @@
+package me.lorenzo.guimanager.opener;
+
+import com.google.common.collect.ImmutableList;
+import me.lorenzo.guimanager.GuiManager;
+import me.lorenzo.guimanager.InventoryManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+import java.util.List;
+
+public class SpecialInventoryOpener implements InventoryOpener {
+
+    private static final List<InventoryType> SUPPORTED = ImmutableList.of(
+            InventoryType.FURNACE,
+            InventoryType.WORKBENCH,
+            InventoryType.DISPENSER,
+            InventoryType.DROPPER,
+            InventoryType.ENCHANTING,
+            InventoryType.BREWING,
+            InventoryType.ANVIL,
+            InventoryType.BEACON,
+            InventoryType.HOPPER
+    );
+
+    @Override
+    public Inventory open(GuiManager inv, Player player) {
+        InventoryManager manager = inv.getParentManager();
+        Inventory handle = Bukkit.createInventory(player, inv.getType(), inv.getTitle());
+
+        fill(handle, manager.getContents(player).get());
+
+        player.openInventory(handle);
+        return handle;
+    }
+
+    @Override
+    public boolean supports(InventoryType type) {
+        return SUPPORTED.contains(type);
+    }
+
+}
